557551474084ec79fd0e30df137b0223454ef5ec
Lars Lam <theveryasian@gmail.com>
2018-09-12 20:38:26 -0700

    git commit start

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+import Component1 from './app/components/Component1/Component1';
+
+export default class myapp extends Component{ 
+    render () {
+      return (
+        <View>
+          <Component1 message = "hello world"/>
+        </View>
+      );
+    }
+
+}
+
+AppRegistry.registerComponent('myapp', ()=> myapp);
---
+++ app/components/Component1/Component1.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+
+
+export default class Component1 extends Component{ 
+
+  constructor() {
+    super ();
+    this.state = {
+    name:'Brad'
+  }
+}
+
+  
+    render (){
+      return (
+        <View>
+          <Text>{this.props.message} </Text>
+          <Text>{this.state.name} </Text>
+        </View>
+      );
+    }
+
+}
+
+AppRegistry.registerComponent('Component1', ()=> Component1);
