23f4b7f41cf1d9ba9c414e0a88ce0dd45b112c5c
Lars Lam <theveryasian@gmail.com>
2018-09-14 23:46:02 -0700

    added new component

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+
 
 import Component1 from './app/components/Component1/Component1';
+
 
 export default class myapp extends Component{ 
     render () {
       return (
         <View>
-          <Component1 message = "hello world"/>
-        </View>
+              <Component1 message="hello world" />
+
+   
+          </View>
       );
     }
 
+ 
 }
 
 AppRegistry.registerComponent('myapp', ()=> myapp);
